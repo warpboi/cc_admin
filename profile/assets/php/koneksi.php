f0fdcb7e8bc00ad8767a3b10f9d1c2af428b6588
MicroWind2 <zfarabi94@gmail.com>
2021-11-10 22:27:17 +0800

    menambahkan profile page, belum masuk database

---
+++ profile/assets/php/koneksi.php
@@ -0,0 +1,11 @@
+<?php
+$connectionString = 'mysql:host=127.0.0.1;dbname=db_fashiondesign';
+try
+{
+$pdo_conn = new PDO($connectionString, 'root', '');
+$pdo_conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    print "Koneksi atau query bermasalah: " . $e->getMessage() . "<br/>";
+    die();
+}
+?>
