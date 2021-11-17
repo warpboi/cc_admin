3d36622392b4a92366ca91ff3269a6993b45abaa
kadabengarann <rio27dh@gmail.com>
2021-11-17 23:25:54 +0800

    add include DB conn

--- pages/admin/user/tambah_user.php
+++ pages/admin/user/tambah_user.php
@@ -1,13 +1,23 @@
 <?php
-if(!empty($_POST["save_record"])) {
-	require_once("koneksi.php");
-	$sql = "INSERT INTO user (nama, username, password, gender) VALUES (:nama,:username,:password,:gender)";
-	$stmt = $pdo_conn->prepare($sql);
-	$result = $stmt->execute( array(':nama'=>$_POST['nama'],':username'=>$_POST['username'], ':password'=>$_POST['password'], ':gender'=>$_POST['gender']) );
-	if (!empty($result) ){
-        header('location:user.php');
-      }else{
-          echo'Gagal menambahkan user';
-      }
+$root = realpath($_SERVER["DOCUMENT_ROOT"]);
+if (strpos($root, 'htdocs') !== false) {
+	$root = $root . '\cc_admin';
 }
-?>
+require_once "$root/app/config/config.php";
+require_once "$root/app/config/Database.php";
+
+$DB = new Database();
+if (!empty($_POST["save_record"])) {
+
+	$sql = "INSERT INTO user (nama, username, password, gender) VALUES (:nama,:username,:password,:gender)";
+	$DB->query($sql);
+	$DB->bind('nama', $_POST['nama']);
+	$DB->bind('password',  $_POST["newPassword"]);
+
+	$DB->execute(array(':nama' => $_POST['nama'], ':username' => $_POST['username'], ':password' => $_POST['password'], ':gender' => $_POST['gender']));
+	if (!empty($result)) {
+		header('location:user.php');
+	} else {
+		echo 'Gagal menambahkan user';
+	}
+}
